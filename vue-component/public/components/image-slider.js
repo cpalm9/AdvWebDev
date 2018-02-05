450cf3a0eec8f7ec0f9a0aaece747604c64f4c72
Chris Palmer <christian_palmer@byu.edu>
2018-02-05 09:54:11 -0700

    Added v-for to display images

--- vue-component/public/components/image-slider.js
+++ vue-component/public/components/image-slider.js
@@ -1,7 +1,8 @@
 Vue.component('image-slider', {
     template: `<div class="image-slider">
-        <div class="images">
+        <div class="images" v-for="img in images">
             <!-- use v-for and v-if to show the correct image here -->
+            <img :src="img">
         </div>
         <div class="image-slider-left" @click="previous"></div>
         <div class="image-slider-right" @click="next"></div>
@@ -9,12 +10,16 @@ Vue.component('image-slider', {
     methods: {
         next: function() {
             // go to the next image
+            
         },
         previous: function() {
             // go to the previous image
+            
         }
     },
     props: {
         // define index and images properties
+        index: Number,
+        images: Array
     }
 });
