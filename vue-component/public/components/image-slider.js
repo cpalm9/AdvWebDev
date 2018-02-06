db663e16875577d68e7fa989fc5ee1044f4b1e89
Chris Palmer <christian_palmer@byu.edu>
2018-02-06 16:47:49 -0700

    Finished vue component assignment

--- vue-component/public/components/image-slider.js
+++ vue-component/public/components/image-slider.js
@@ -1,8 +1,8 @@
 Vue.component('image-slider', {
     template: `<div class="image-slider">
-        <div class="images" v-for="img in images">
+        <div class="images" v-for="img in [index]">
             <!-- use v-for and v-if to show the correct image here -->
-            <img :src="img">
+            <img :src="images[index]">
         </div>
         <div class="image-slider-left" @click="previous"></div>
         <div class="image-slider-right" @click="next"></div>
@@ -10,11 +10,29 @@ Vue.component('image-slider', {
     methods: {
         next: function() {
             // go to the next image
-            
+            let index = this.index;
+            if(index === this.images.length - 1){
+                index = 0;
+                setTimeout( ()=>{
+                    this.$emit('index-change', index);
+                })
+            }
+            else{
+                index += 1
+                this.$emit('index-change', index)
+            }
         },
         previous: function() {
             // go to the previous image
-            
+            let index = this.index;
+            if(index === 0){
+                index = this.images.length - 1;
+                this.$emit('index-change', index);
+            }
+            else{
+                index -= 1;
+                this.$emit('index-change', index);
+            }
         }
     },
     props: {
